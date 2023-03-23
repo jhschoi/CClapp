3ca3b0f7da3cfc0881b15013ee24186ea3f37747
jhschoi <jhschoi@uchicago.edu>
2023-03-23 14:08:11 -0500

    New Class Testing Code

---
+++ class_2_testing.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author:Joseph Choi
+"""
+
+# This is a 2nd class live lab code. We use this for testing Github and Spyder
+
+print("Hello, world. This is a test program.")
+
+print("We want to show that 10 + 12 is")
+x = 10
+y = 12
+print(x+y)
